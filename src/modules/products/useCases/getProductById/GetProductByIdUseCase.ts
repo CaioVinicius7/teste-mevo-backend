a09ea28d548a5947c4e662171ea8b5257a0fb33e
Caio Vinícius <caio1525pereira@gmail.com>
2022-11-16 16:44:04 -0300

    refactor: :hammer: Lançamento de exceção ao não encontrar nenhum produto com o id informado

--- src/modules/products/useCases/getProductById/GetProductByIdUseCase.ts
+++ src/modules/products/useCases/getProductById/GetProductByIdUseCase.ts
@@ -2,6 +2,7 @@ import { injectable, inject } from "tsyringe";
 
 import { Product } from "@prisma/client";
 
+import { AppError } from "../../../../shared/errors/AppError";
 import { IProductsRepository } from "../../repositories/IProductsRepository";
 
 @injectable()
@@ -13,6 +14,10 @@ export class GetProductByIdUseCase {
 
 	async execute(id: string): Promise<Product | null> {
 		const product = await this.productsRepository.getById(id);
+
+		if (!product) {
+			throw new AppError("Product not found", 404);
+		}
 
 		return product;
 	}
