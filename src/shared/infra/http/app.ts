17eb3c283549bf3884c00cf7a6b072f96c973bec
Caio Vinícius <caio1525pereira@gmail.com>
2022-11-16 16:43:28 -0300

    feat: :goal_net: Implementação de método tratativa de exceções personalizadas

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,16 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import "dotenv/config";
 import "reflect-metadata";
+import "express-async-errors";
 
 import cors from "cors";
-import Express from "express";
+import Express, { NextFunction, Request, Response } from "express";
 
-import "../../container";
+import { AppError } from "../../errors/AppError";
 import { router } from "./routes/index";
+import "../../container";
 
 const app = Express();
 
 app.use(cors());
 app.use(Express.json());
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+	if (err instanceof AppError) {
+		return res.status(err.statusCode).json({
+			message: err.message
+		});
+	}
+});
 
 export { app };
