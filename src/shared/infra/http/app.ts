ac47a1448b9a4bb51030b653174cf3118f7d983c
Caio Vinícius <caio1525pereira@gmail.com>
2022-11-17 13:37:17 -0300

    docs: :memo: Documentação de endpoints com o swagger

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -5,15 +5,27 @@ import "express-async-errors";
 
 import cors from "cors";
 import Express, { NextFunction, Request, Response } from "express";
+import swaggerUi from "swagger-ui-express";
 
+import swaggerFile from "../../../swagger.json";
 import { AppError } from "../../errors/AppError";
 import { router } from "./routes/index";
+
 import "../../container";
+
+const options = {
+	swaggerOptions: {
+		url: "http://petstore.swagger.io/v2/swagger.json"
+	}
+};
 
 const app = Express();
 
 app.use(cors());
 app.use(Express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.use(router);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
